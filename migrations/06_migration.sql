dfe9ca10c034f313486bb24f5e476cbbdbb9496d
Evgeny Egorov <heaventud@gmail.com>
2026-08-16 18:31:20 +0300

    add solution for task 1

---
+++ migrations/06_migration.sql
@@ -0,0 +1,57 @@
+CREATE TABLE tbl1
+(
+    UserID UInt64,
+    PageViews UInt8,
+    Duration UInt8,
+    Sign Int8,
+    Version UInt8
+)
+ENGINE = VersionedCollapsingMergeTree(Sign, Version)
+ORDER BY UserID;
+
+CREATE TABLE tbl2
+(
+    key UInt32,
+    value UInt32
+)
+ENGINE = SummingMergeTree(value)
+ORDER BY key;
+
+CREATE TABLE tbl3
+(
+    `id` Int32,
+    `status` String,
+    `price` String,
+    `comment` String
+)
+ENGINE = ReplacingMergeTree
+PRIMARY KEY (id)
+ORDER BY (id, status);
+
+CREATE TABLE tbl4
+(   CounterID UInt8,
+    StartDate Date,
+    UserID UInt64
+) ENGINE = MergeTree
+PARTITION BY toYYYYMM(StartDate)
+ORDER BY (CounterID, StartDate);
+
+CREATE TABLE tbl5
+(   CounterID UInt8,
+    StartDate Date,
+    UserID AggregateFunction(uniq, UInt64)
+) ENGINE = AggregatingMergeTree
+PARTITION BY toYYYYMM(StartDate)
+ORDER BY (CounterID, StartDate);
+
+CREATE TABLE tbl6
+(
+    id Int32,
+    status String,
+    price String,
+    comment String,
+    Sign Int8
+)
+ENGINE = CollapsingMergeTree(Sign)
+PRIMARY KEY (id)
+ORDER BY (id, status);
